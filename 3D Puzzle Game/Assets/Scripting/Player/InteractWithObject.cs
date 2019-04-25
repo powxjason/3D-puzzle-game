5a192d1d47e8ce0ca9ee4d9b1dbb3f0d50d12aee
Jason Sivak <20.jason.sivak@ksd.org>
2019-04-25 13:54:58 -0700

    Added Interact Symbol

---
+++ 3D Puzzle Game/Assets/Scripting/Player/InteractWithObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithObject : MonoBehaviour
+{
+    public GameObject InteractSymbol;
+    public GameObject PointingAt;
+    Camera camera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTarget();
+        OpenDoors();
+    }
+
+    void CheckTarget()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit))
+        {
+
+            PointingAt = hit.transform.gameObject;
+
+        }
+    }
+
+    void OpenDoors()
+    {
+
+        if (((PointingAt.gameObject.GetComponent("DoorLift") as DoorLift) != null))
+        {
+            InteractSymbol.SetActive(true);
+
+            if (Input.GetButtonDown("Fire1")){
+
+                PointingAt.GetComponent<DoorLift>().RaiseDoor();
+
+            }                    
+        }else
+        {
+            InteractSymbol.SetActive(false);
+        }
+    }
+}
