4a8cd139f3db4c3c1e55456373e3f1226a4b3660
Jason Sivak <20.jason.sivak@ksd.org>
2019-05-06 13:28:38 -0700

    First Puzzle Works

--- 3D Puzzle Game/Assets/Scripting/Enviorment/LoadAndUnload.cs
+++ 3D Puzzle Game/Assets/Scripting/Enviorment/LoadAndUnload.cs
@@ -8,16 +8,26 @@ public class LoadAndUnload : MonoBehaviour
 
     public GameObject[] UnloadGameObject;
 
-    void Activate()
+    void OnTriggerEnter(Collider thing)
     {
+
+        if(thing.gameObject.tag == "Player")
+        {
+        ForLoop(true, LoadGameObject);
+        ForLoop(false, UnloadGameObject);
+        }
 
     }
 
     void ForLoop(bool Enable, GameObject[] Array)
     {
-        for(int i = 1; i <= Array.Length + 1; i++)
+        for(int i = 0; i <= Array.Length - 1; i++)
         {
-
+            Debug.Log(i + "," + Enable + "," + Array);
+            if(Array[i] != null)
+            {
+                Array[i].SetActive(Enable);
+            }
         }
     }
 
