686e0d07a57c25d6587cb38ffc75a5d0454dcd3c
Gwendolyn Farrar <19.gwendolyn.farrar@ksd.org>
2019-05-16 13:21:47 -0700

    pause and main menu

---
+++ 3D Puzzle Game/Assets/Scripting/MainMenuButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
