e2258e22e6985feb4405b5fc60e232ec3dff5b85
Jason Sivak <20.jason.sivak@ksd.org>
2019-05-16 11:44:45 -0700

    Queue the boss music

--- 3D Puzzle Game/Assets/Scripting/Enviorment/LevelChange.cs
+++ 3D Puzzle Game/Assets/Scripting/Enviorment/LevelChange.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public string LevelString;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+
+        if(LevelString != null)
+        {
+            SceneManager.LoadScene(LevelString);
+        }
+        else
+        {
+            Debug.Log("No Level Selected");
+        }
     }
 }
